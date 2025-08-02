e59a41dbdf204bfdd1f4c0adf20aca5d439f85d2
Matheus Zanutto Gaspar <matszg@live.com>
2025-08-02 15:16:58 -0300

    feat: product service

---
+++ internal/product/product_service.go
@@ -0,0 +1,61 @@
+package product
+
+import (
+	"errors"
+	"strings"
+
+	"gorm.io/gorm"
+)
+
+func ListProducts(db *gorm.DB, nameFilter, categoryFilter string) ([]Product, error) {
+	var products []Product
+	query := db.Model(&Product{})
+
+	if nameFilter != "" {
+		query = query.Where("LOWER(name) LIKE ?", "%"+strings.ToLower(nameFilter)+"%")
+	}
+
+	if categoryFilter != "" {
+		query = query.Where("LOWER(category) = ?", strings.ToLower(categoryFilter))
+	}
+
+	if err := query.Find(&products).Error; err != nil {
+		return nil, err
+	}
+
+	return products, nil
+}
+
+func UpdateProduct(db *gorm.DB, updated Product) (Product, error) {
+	if updated.ID == 0 || updated.Name == "" || updated.Price <= 0 {
+		return Product{}, errors.New("invalid product data")
+	}
+
+	var existing Product
+	if err := db.First(&existing, updated.ID).Error; err != nil {
+		return Product{}, err
+	}
+
+	existing.Name = updated.Name
+	existing.Desc = updated.Desc
+	existing.Price = updated.Price
+	existing.Category = updated.Category
+
+	if err := db.Save(&existing).Error; err != nil {
+		return Product{}, err
+	}
+
+	return existing, nil
+}
+
+func DeleteProduct(db *gorm.DB, id uint) error {
+	if id == 0 {
+		return errors.New("invalid ID")
+	}
+
+	if err := db.Delete(&Product{}, id).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
